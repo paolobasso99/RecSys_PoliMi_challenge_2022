6ddf39d91ef109476463913cf65d94e78fbc0796
Paolo Basso <bassopaolo99@gmail.com>
2023-01-13 13:13:30 +0100

    MF_SDVpp

--- run_MF_SDVpp_training.py
+++ run_MF_SDVpp_training.py
@@ -1,7 +1,7 @@
 import os
 from data_manager import DatasetLoader, DatasetSplitter, URMGenerator
 from evaluation.evaluator import EvaluatorHoldout
-from Recommenders.MatrixFactorization.Cython.MatrixFactorization_Cython import MatrixFactorization_AsySVD_Cython
+from Recommenders.MatrixFactorization.Cython.MatrixFactorization_Cython import MatrixFactorization_SVDpp_Cython
 from skopt.space import Real, Integer, Categorical
 from HyperparameterTuning.SearchBayesianSkopt import SearchBayesianSkopt
 from HyperparameterTuning.SearchAbstractClass import SearchInputRecommenderArgs
@@ -12,14 +12,16 @@ if __name__ == "__main__":
     dataset_splitter = DatasetSplitter(dataset_loader)
     dataset_train, dataset_val = dataset_splitter.load_interactions_train_val()
     URM_generator = URMGenerator(dataset_train, dataset_val)
-    URM_train, URM_val = URM_generator.generate_explicit_URM(log_base=4, views_weight=1, details_weight=0.8)
+    URM_train, URM_val = URM_generator.generate_explicit_URM(
+        log_base=4, views_weight=1, details_weight=0.8
+    )
     URM_all = URM_train + URM_val
 
     evaluator = EvaluatorHoldout(URM_val, cutoff_list=[10])
-    
-    output_folder_path = "result_experiments/MF_AsySVD/"
-    recommender_class = MatrixFactorization_AsySVD_Cython
-    n_cases = 50
+
+    output_folder_path = "result_experiments/MF_SVDpp/"
+    recommender_class = MatrixFactorization_SVDpp_Cython
+    n_cases = 100
     n_random_starts = int(n_cases * 0.3)
     metric_to_optimize = "MAP"
     cutoff_to_optimize = 10
@@ -31,14 +33,15 @@ if __name__ == "__main__":
     # Define hyperparameters
     hyperparameters_range_dictionary = {
         "sgd_mode": Categorical(["sgd", "adagrad", "adam"]),
-        "epochs": Categorical([200]),
+        "epochs": Categorical([500]),
         "use_bias": Categorical([True, False]),
-        "batch_size": Categorical([1]),
+        "batch_size": Categorical([1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]),
         "num_factors": Integer(1, 200),
-        "item_reg": Real(low=1e-5, high=1e-2, prior="log-uniform"),
-        "user_reg": Real(low=1e-5, high=1e-2, prior="log-uniform"),
-        "learning_rate": Real(low=1e-4, high=1e-1, prior="log-uniform"),
-        "negative_interactions_quota": Real(low=0.0, high=0.5, prior="uniform"),
+        "item_reg": Real(low = 1e-5, high = 1e-2, prior = 'log-uniform'),
+        "user_reg": Real(low = 1e-5, high = 1e-2, prior = 'log-uniform'),
+        "learning_rate": Real(low = 1e-4, high = 1e-1, prior = 'log-uniform'),
+        "negative_interactions_quota": Real(low = 0.0, high = 0.5, prior = 'uniform'),
+        "dropout_quota": Real(low = 0.01, high = 0.7, prior = 'uniform')
     }
 
     hyperparameter_search = SearchBayesianSkopt(
@@ -46,14 +49,24 @@ if __name__ == "__main__":
         evaluator_validation=evaluator,
     )
 
+    earlystopping_keywargs = {
+        "validation_every_n": 5,
+        "stop_on_validation": True,
+        "evaluator_object": evaluator,
+        "lower_validations_allowed": 5,
+        "validation_metric": metric_to_optimize,
+    }
+
     recommender_input_args = SearchInputRecommenderArgs(
-        CONSTRUCTOR_POSITIONAL_ARGS=[
-            URM_train,
-        ],
+        CONSTRUCTOR_POSITIONAL_ARGS=[URM_train],
         CONSTRUCTOR_KEYWORD_ARGS={},
         FIT_POSITIONAL_ARGS=[],
-        FIT_KEYWORD_ARGS={},
-        EARLYSTOPPING_KEYWORD_ARGS={},
+        FIT_KEYWORD_ARGS={
+            "positive_threshold_BPR": None,
+            "train_with_sparse_weights": False,
+            "allow_train_with_sparse_weights": False,
+        },
+        EARLYSTOPPING_KEYWORD_ARGS=earlystopping_keywargs,
     )
 
     if True:
@@ -76,7 +89,7 @@ if __name__ == "__main__":
 
     best_hyperparameters = search_metadata["hyperparameters_best"]
     print("best_param", best_hyperparameters)
-    
+
     recommender = recommender_class(URM_train + URM_val)
     recommender.fit(**best_hyperparameters)
     recommender.save_model(
